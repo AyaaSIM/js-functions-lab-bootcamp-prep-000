da0f416601aef55f4ec29d54bcdb85b01eea4968
Ayaa <ayaa1siddig5@gmail.com>
2019-03-05 06:31:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function happyHolidays() {
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 } 
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountDown(holiday, days){
+  returns `Its\s ${days} days until ${holiday}!`
+}
